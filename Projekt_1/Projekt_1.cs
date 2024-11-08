71acf0341d09c89e663ad3b0a31e014c1d0d951a
Kamil Kumszczyński <92692310+perlamiud@users.noreply.github.com>
2024-11-08 10:19:56 +0100

    commit drugi

--- Projekt_1/Projekt_1.cs
+++ Projekt_1/Projekt_1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
-using Projekt1_Mickiewicz71500;
+using Projekt1_Kumszczynski71523;
 
 
 /*
